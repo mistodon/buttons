6698b59a746f7a611e4cd7980376e2487490d169
Claire Harris <wishing.engine@gmail.com>
2018-08-08 15:14:08 +0100

    Switch over to generic representation of mouse/keyboard

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,5 @@ mod winit_support;
 mod keyboard;
 mod mouse;
 
-pub use keyboard::{KeyId, Keyboard, KeyboardInput, Modifiers};
-pub use mouse::{Mouse, MouseButtonId, MouseInput};
+pub use keyboard::{Keyboard, KeyboardInput};
+pub use mouse::{Mouse, MouseInput};
